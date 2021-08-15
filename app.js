4f1c0705c33665dbc762116bfe6fb6b87339ef7f
Carlisle Payne <87280475+CPayne617@users.noreply.github.com>
2021-08-15 18:41:01 -0400

    Connected JS with html and added EventListener
    used querySelector to link class box & make all the boxes click sensitive and log box in the console

---
+++ app.js
@@ -0,0 +1,8 @@
+//box elements from html to be stored as array-like object
+const boxes = document.querySelector(".box") 
+
+
+gameboard.addEventListener("click", e => {
+   console.log("box")
+})
+// starting game
