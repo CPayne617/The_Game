173ef703ddbdded3f91b083497bc8de80958540b
Carlisle Payne <87280475+CPayne617@users.noreply.github.com>
2021-08-17 22:41:21 -0400

    changed console log message & started 1st of questions
    I changed the console.log from "box" to e.target.id to get the ID numbers of the individual box elements. also started writing my 1st function and it seems to be working fine. Next I have to setup the pop up function so the questions get displayed and user will be able to input their answers

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const boxes = document.querySelector(".box")
 
 
 gameboard.addEventListener("click", e => {
-   console.log("box")
+   console.log(e.target.id);
 })
+
+
+const HundredSq = [{
+   QuestOne: "What are Spinners?",
+   ptValue: 100,
+   answer: "Car rims"
+}]
+if(userInput === HundredSq[0].answer){
+   console.log("Correct")
+}
+// class Hoodpardy {
+//    constructor(element, options = {}){
+//       this.useCategoryIds = [];
+//    }
+// }
 // starting game
+// boxes.innerHTML = 
+
+// const game = new Hoodpardy
